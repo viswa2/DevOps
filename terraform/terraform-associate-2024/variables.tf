1dfc3cd6be9fbe7db414faa6ec8e31920e00f871
Viswanatha Reddy <mekala.viswanathreddy@gmail.com>
2024-07-09 15:57:07 +0530

    Added learnings for terraform-associate-2024

--- terraform/terraform-associate-2024/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key_id" {
-  description = "Value of the iam user access key id"
-  type        = string
-}
-
-variable "aws_secret_access_key" {
-  description = "Value of the iam user secret key id"
-  type        = string
-}
